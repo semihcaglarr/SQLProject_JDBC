c0590529bafe55615c97c50317faa59b23ec6133
semihcaglarr <caglarsemih1@outlook.com>
2024-05-03 05:50:45 +0300

    Task 25 from automated SQL queries with JDBC formatted

--- src/test/java/SQL_Tasks/Tasks_25_between_32.java
+++ src/test/java/SQL_Tasks/Tasks_25_between_32.java
@@ -1,8 +1,27 @@
 package SQL_Tasks;
 
 import Utilities.DBUtility;
+import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class Tasks_25_between_32 extends DBUtility {
+
+    @Test
+    public void Task25(){
+        List<List<String>> dbList = getListData("SELECT * FROM employees WHERE first_name = 'Annemarie' AND last_name = 'Redmiles';");
+
+        System.out.println("**** List the information (employee number, date of birth, first name, last name, gender, hire date) ****");
+        System.out.println("**** Of the employee named 'Annemarie Redmiles' ****");
+        System.out.println();
+
+        for (int i = 0; i < dbList.size(); i++) {
+            for (int j = 0; j < dbList.get(i).size(); j++) {
+                System.out.print(dbList.get(i).get(j) + " ");
+            }
+            System.out.println();
+        }
+    }
 
 
 }
